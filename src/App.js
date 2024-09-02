405bce33c4136bd0225b666221ea639583b13630
Purnima Lakshminarayanan <purnima.nyc@gmail.com>
2024-09-02 11:14:17 -0400

    square displays X when clicked

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function Square() {
   const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log("clicked");
+    setValue("X");
   }
   return (
     <button className="square" onClick={handleClick}>
@@ -14,6 +14,8 @@ function Square() {
 }
 
 export default function Board() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
