bbc9096853d8f1988ee30d34af9468c867417434
Purnima Lakshminarayanan <purnima.nyc@gmail.com>
2024-09-09 12:36:48 -0400

    add functionality for Square to update Board's state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import { useState } from "react";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square({ value, onBoardClick }) {
+  return (
+    <button className="square" onClick={onBoardClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   const [board, setBoard] = useState(Array(9).fill(null));
 
+  function handleClick() {
+    console.log("the board was clicked!");
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={board[0]} />
-        <Square value={board[1]} />
-        <Square value={board[2]} />
+        <Square value={board[0]} onBoardClick={handleClick} />
+        <Square value={board[1]} onBoardClick={handleClick} />
+        <Square value={board[2]} onBoardClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={board[3]} />
-        <Square value={board[4]} />
-        <Square value={board[5]} />
+        <Square value={board[3]} onBoardClick={handleClick} />
+        <Square value={board[4]} onBoardClick={handleClick} />
+        <Square value={board[5]} onBoardClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={board[6]} />
-        <Square value={board[7]} />
-        <Square value={board[8]} />
+        <Square value={board[6]} onBoardClick={handleClick} />
+        <Square value={board[7]} onBoardClick={handleClick} />
+        <Square value={board[8]} onBoardClick={handleClick} />
       </div>
     </>
   );
